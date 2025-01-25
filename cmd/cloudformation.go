7806f65fd4f4deb9bc52de9feed7283d4ebee040
Asaf David <asaf.david@earnix.com>
2025-01-25 21:01:25 +0200

    Add list-cidr command

---
+++ cmd/cloudformation.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// iamCmd represents the iam command
+var cfCmd = &cobra.Command{
+	Use:   "cloudformation",
+	Short: "Manage Supported Infrastructure as Code with CloudFormation",
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cfCmd)
+	cfCmd.AddCommand(createAssumedRoleCmd)
+	cfCmd.AddCommand(createTableCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// iamCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// iamCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/dynamodb.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// dynamodbCmd represents the dynamodb command
+var dynamodbCmd = &cobra.Command{
+	Use:   "dynamodb",
+	Short: "Manage DynamoDB Operations",
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(dynamodbCmd)
+	dynamodbCmd.AddCommand(createTableCmd)
+	dynamodbCmd.AddCommand(importCidrCmd)
+	dynamodbCmd.AddCommand(listCidrCmd)
+	dynamodbCmd.AddCommand(releaseCidrCmd)
+	dynamodbCmd.AddCommand(reserveCidrCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// dynamodbCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// dynamodbCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
